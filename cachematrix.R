dfb51f46190692bd65838996078900fcb73b9dbb
Hanae Casseres <hcasseres@gmail.com>
2014-08-23 17:43:33 -0700

    add a lot of comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,39 @@
 ## Acknowledgement of external sources: I used the class forums, especially
 ## Bill Hilton's posts, to help me understand this assignment.
 
+# Given a matrix as input, makeCacheMatrix and cacheSolve work together to 
+# calculate and cache the inverse of a function. If the inverse has already been
+# cached, cacheSolve returns the cached value instead of recalculating it.
+
 ## makeCacheMatrix takes a matrix as input. Its output is an object containing a
 ## list of functions (get, set, setinv, and getinv).
+
+## If we use <- (assignment) instead of <<- (superassignment) in setinv(), the 
+## inverse is not cached. We need to use superassignment in order to set inv in
+## the environment of makeCacheMatrix() instead of setinv().
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
+    inv <- NULL # will hold the cached inverse of x
     set <- function(y) {
-        x <<- y
-        inv <<- NULL # if matrix changes, reset the inverse to null
+        x <<- y # reset the matrix to y
+        inv <<- NULL # if matrix changes, reset the inverse to NULL
         }
     get <- function () x # return the original matrix
-    setinv <- function(solve) inv <<- solve # will be called by cacheSolve
+    setinv <- function(newinv) inv <<- newinv # will be called by cacheSolve
     getinv <- function() inv # return the inverse matrix
-    list(get = get, # return the methods in this object
+    list(get = get, # creates a list of the methods in this object
         set = set,
         setinv = setinv,
         getinv = getinv)
 }
 
-## cacheSolve takes as input the object created by makeCacheMatrix. If the
-## inverse was already calculated, it returns the cached value. Otherwise, it
-## calculates the inverse of the matrix that was the input to makeCacheMatrix.
 
-## If the matrix is changed via set(), inv is also reset to null, so cacheSolve
-## will not return the old inverse.
+## The input to cacheSolve is an object created by makeCacheMatrix. If the 
+## inverse was already calculated, cacheSolve returns the cached value.
+## Otherwise, it calculates the inverse of the matrix that was the input to
+## makeCacheMatrix.
+
+## If the matrix is changed via set(), inv is also reset to NULL, so cacheSolve
+## will recalculate the inverse.
 
 cacheSolve <- function(x, ...) {
     # check for an existing solution
@@ -34,7 +44,7 @@ cacheSolve <- function(x, ...) {
     }
     # if there was no cached solution, calculate the inverse
     orig <- x$get() # get the original matrix
-    inv <- solve(orig)
-    x$setinv(inv) 
+    inv <- solve(orig) # find the solution
+    x$setinv(inv) # use setinv() to cache the solution
     inv
 }
